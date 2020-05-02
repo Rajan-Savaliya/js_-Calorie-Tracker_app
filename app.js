806d9ddea51cc9e35a80f6db3be5f48dd5ccb2dd
rajan savaliya <rajansavaliya6328@gmail.com>
2020-05-03 00:44:08 +0530

    add 6

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const StorageCtrl = (function(){
   return {
     storeItem: function(item){
       let items;
+      // Check if any items in ls
+      if(localStorage.getItem('items') === null){
+        items = [];
+        // Push new item
+        items.push(item);
+        // Set ls
+        localStorage.setItem('items', JSON.stringify(items));
